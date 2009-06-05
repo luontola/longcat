f6cdab3f39a621912aa1bb36e12648641f172d13
Esko Luontola <esko.luontola@gmail.com>
2009-06-05 20:34:05 +0300

    User friendly error messages

--- src/main/java/longcat/Cli.java
+++ src/main/java/longcat/Cli.java
@@ -1,8 +1,14 @@
 package longcat;
 
 public class Cli {
+
     public static void main(String[] args) {
-        System.out.print(build().parseArguments(args));
+        try {
+            System.out.print(build().parseArguments(args));
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
 
     private static CliArgumentParser build() {
